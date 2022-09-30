996516b4827b6948e10d4e5ec2d0729ca20ea315
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-30 17:24:17 +0900

    [VisuallyHidden] a11yフォルダへ移動

--- src/lib/FloatLabelInput.svelte
+++ src/lib/FloatLabelInput.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import VisuallyHidden from "@/lib/VisuallyHidden.svelte"
+import VisuallyHidden from "@/lib/a11y/VisuallyHidden.svelte"
 
 export let id: string
 export let label: string
