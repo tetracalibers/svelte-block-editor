42d28a65fc27fe1adcdb93802a3c83798ae5ebea
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-30 18:52:54 +0900

    [IconButton] 実装

---
+++ src/lib/a11y/IconButton.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import VisuallyHidden from "./VisuallyHidden.svelte"
+
+export let label: string
+export let type: "reset" | "button" | "submit" = "button"
+</script>
+
+<button type="{type}">
+  <slot name="icon" aria-hidden="true" />
+  <VisuallyHidden>{label}</VisuallyHidden>
+</button>
+
+<style lang="stylus">
+button
+  background-color: rgba(255, 255, 255, 0)
+  border: none
+  cursor: pointer
+  outline: none
+  padding: 0
+  appearance: none
+  font-size: 1rem
+</style>
