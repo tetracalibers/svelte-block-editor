8d7ce10bed77b33632c1e1f44bf4e48af7b3d9b5
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-30 14:31:14 +0900

    [BlockEditor] 背景設定

--- src/lib/BlockEditor.svelte
+++ src/lib/BlockEditor.svelte
@@ -2,4 +2,20 @@
 import FloatLabelInput from "@/lib/FloatLabelInput.svelte"
 </script>
 
-<FloatLabelInput id="test" label="test" />
+<div class="block-editor">
+  <FloatLabelInput id="test" label="test" />
+</div>
+
+<style>
+.block-editor {
+  background-image: linear-gradient(
+    -225deg,
+    #5d9fff 0%,
+    #b8dcff 48%,
+    #6bbbff 100%
+  );
+  padding: 2rem;
+  margin: 1rem;
+  border-radius: 1rem;
+}
+</style>
